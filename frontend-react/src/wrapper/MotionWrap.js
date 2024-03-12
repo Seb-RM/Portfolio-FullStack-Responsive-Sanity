8de4fe14345b2321812817ba013e1242c80a97e5
Seb-RM <122194477+Seb-RM@users.noreply.github.com>
2024-03-11 23:03:32 -0300

    configuracion footer y contacto

--- frontend-react/src/wrapper/MotionWrap.js
+++ frontend-react/src/wrapper/MotionWrap.js
@@ -7,7 +7,7 @@ const MotionWrap = (Component, classNames ) => function HOC() {
         <motion.div
         whileInView={{y:[100, 50, 0], opacity:[0,0,1]}}
         transition={{duration:0.5}}
-        className={`${classNames} app_flex`}
+        className={`${classNames} app__flex`}
         >
             <Component/>
         </motion.div>
